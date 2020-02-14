0b7157dbd9e3c43f994adc786928bea52bb0bbb3
Lindroy <linyulong2014@163.com>
2020-02-14 15:52:02 +0800

    初始设置方法添加注释。

--- multistatelayout/src/main/java/com/lindroy/multistatelayout/MultiStateLayout.kt
+++ multistatelayout/src/main/java/com/lindroy/multistatelayout/MultiStateLayout.kt
@@ -1,6 +1,7 @@
 package com.lindroy.multistatelayout
 
 import androidx.annotation.IdRes
+import androidx.annotation.IntRange
 import androidx.annotation.LayoutRes
 import com.lindroy.multistatelayout.bean.StateInfo
 import com.lindroy.multistatelayout.constants.NULL_RESOURCE_ID
@@ -40,19 +41,39 @@ class MultiStateLayout internal constructor() {
     internal val noNetworkInfo: StateInfo
         get() = getStateView(STATE_NO_NETWORK)
 
+    /**
+     * 设置加载中视图
+     * @param layoutId:布局Id
+     * @param hintTextId:用于显示提示文字的控件Id
+     * @param hintText:提示文字
+     */
     fun setLoadingView(
         @LayoutRes layoutId: Int,
         @IdRes hintTextId: Int = NULL_RESOURCE_ID,
         hintText: String? = null
     ) = addStateViewToList(STATE_LOADING, layoutId, hintTextId, hintText)
 
+    /**
+     * 设置空数据视图
+     * @param layoutId:布局Id
+     * @param hintTextId:用于显示提示文字的控件Id
+     * @param hintText:提示文字
+     * @param clickViewIds:需要设置点击事件的控件Id
+     */
     fun setEmptyView(
         @LayoutRes layoutId: Int,
         @IdRes hintTextId: Int = NULL_RESOURCE_ID,
-        hintText: String? =null,
+        hintText: String? = null,
         @IdRes vararg clickViewIds: Int
     ) = addStateViewToList(STATE_EMPTY, layoutId, hintTextId, hintText, *clickViewIds)
 
+    /**
+     * 设置错误视图
+     * @param layoutId:布局Id
+     * @param hintTextId:用于显示提示文字的控件Id
+     * @param hintText:提示文字
+     * @param clickViewIds:需要设置点击事件的控件Id
+     */
     fun setErrorView(
         @LayoutRes layoutId: Int,
         @IdRes hintTextId: Int = NULL_RESOURCE_ID,
@@ -60,6 +81,13 @@ class MultiStateLayout internal constructor() {
         @IdRes vararg clickViewIds: Int
     ) = addStateViewToList(STATE_ERROR, layoutId, hintTextId, hintText, *clickViewIds)
 
+    /**
+     * 设置网络断开视图
+     * @param layoutId:布局Id
+     * @param hintTextId:用于显示提示文字的控件Id
+     * @param hintText:提示文字
+     * @param clickViewIds:需要设置点击事件的控件Id
+     */
     fun setNoNetworkView(
         @LayoutRes layoutId: Int,
         @IdRes hintTextId: Int = NULL_RESOURCE_ID,
@@ -67,8 +95,14 @@ class MultiStateLayout internal constructor() {
         @IdRes vararg clickViewIds: Int
     ) = addStateViewToList(STATE_NO_NETWORK, layoutId, hintTextId, hintText, *clickViewIds)
 
-    fun addStateView(status: Int, @LayoutRes layoutId: Int, @IdRes vararg clickViewIds: Int) =
-        addStateViewToList(status, layoutId, clickViewIds = *clickViewIds)
+    /**
+     * 添加自定义视图
+     * @param state:视图状态值，取值时
+     * @param layoutId:布局Id
+     * @param clickViewIds:需要设置点击事件的控件Id
+     */
+    fun addStateView(@IntRange(from = 5L)  state: Int, @LayoutRes layoutId: Int, @IdRes vararg clickViewIds: Int) =
+        addStateViewToList(state, layoutId, clickViewIds = *clickViewIds)
 
     internal fun getStateView(status: Int) = stateViewList.firstOrNull { it.state == status }
         ?: StateInfo(status)
@@ -77,7 +111,7 @@ class MultiStateLayout internal constructor() {
         status: Int,
         @LayoutRes layoutId: Int,
         @IdRes hintId: Int = NULL_RESOURCE_ID,
-        hintText: String? =null,
+        hintText: String? = null,
         @IdRes vararg clickViewIds: Int
     ) =
         this.apply {
